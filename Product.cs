b2f221214cc10314d93c07a0bc94161f2b19c56e
Priyankahirugade <128826836+Priyankahirugade@users.noreply.github.com>
2023-04-04 00:14:00 -0700

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace CodeFirst.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required(ErrorMessage ="product name required")]
+        [DisplayName("product Name")]
+        public string ProductName { get; set; }
+
+        [Required(ErrorMessage = "price is required")]
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage = "Qty is required")]
+        public int Qty { get; set; }
+
+        public string Remarks { get; set; }
+
+    }
+
+    public class EFCodeFirstEntity : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
